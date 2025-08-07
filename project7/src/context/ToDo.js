dd49e28616633f0d802b00b8e5432133abcd9b2f
AtharvaVKadam <atharvak731@gmail.com>
2025-08-07 23:47:33 +0530

    To Do

---
+++ project7/src/context/ToDo.js
@@ -0,0 +1,21 @@
+import { useContext, createContext } from  "react";
+
+export const TodoContext = createContext({
+    todos : [{
+        id : 1,
+        todo : "to do mssg",
+        completed : false,
+    }
+],
+    addTodo : (todo) => {},
+    updateTodo : (id,todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {},
+})
+
+export const useToDo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todorovider = TodoContext.Provider
+
---
+++ project7/src/context/index.js
@@ -0,0 +1,1 @@
+import { TodoContext, TodoProvider ,useToDo } from "./ToDo";
